6d5a71251c6b457244bff104e1384bc9d3f2c974
yakuperenermurat <yakuperenermurat@gmail.com>
2024-05-10 16:12:11 +0300

    mine board updates

--- MineSweeper/src/MineSweeper.java
+++ MineSweeper/src/MineSweeper.java
@@ -22,6 +22,7 @@ public class MineSweeper {
         initializeBoard();
         placeMines();
         calculateNumbers();
+        printBoard(mineBoard);
     }
 
     // Initializes the game boards to their initial state
